e7e7dd8f810f191911b724746484b0f09e8ee6fd
Jr-Yung Tsai <wemee7012@gmail.com>
2018-08-28 16:05:06 +0800

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,108 @@
+$BOARD_SIZE = {width: 10, height: 10}
+$BOARD = Array.new($BOARD_SIZE[:width]) {Array.new($BOARD_SIZE[:height]) {'.'}}
+
+$SHIPS = [{
+    name: "航空母艦",
+    label: :A,
+    size: 5}, {
+    name: "戰鬥艦",
+    label: :B,
+    size: 4}, {
+    name: "巡洋艦",
+    label: :C,
+    size: 3}, {
+    name: "潛水艇",
+    label: :S,
+    size: 3}, {
+    name: "驅逐艦",
+    label: :D,
+    size: 2}]
+
+def get_rand_pos w=$BOARD_SIZE[:width], h=$BOARD_SIZE[:height]
+  {x: rand(w), y: rand(h)}
+end
+
+# {:name=>"驅逐艦", :label=>:D, :size=>2}
+# {:x=>0, :y=>6}
+# :r
+
+def deploy ship
+  p ship
+  pos = get_rand_pos
+  row_or_column = [:r, :c].sample
+  if(row_or_column==:r)
+    boundary = pos[:x]+ship[:size]-1
+    if boundary >= $BOARD_SIZE[:width]
+      deploy ship
+    else
+      op = true
+      (pos[:x]..(pos[:x]+ship[:size]-1)).each{|x|
+        unless $BOARD[x][pos[:y]]=='.'
+          op = false
+          break
+        end
+      }
+      if op
+        (pos[:x]..(pos[:x]+ship[:size]-1)).each{|x|
+          $BOARD[x][pos[:y]] = ship[:label]
+        }
+      else
+        deploy ship
+      end
+    end
+  else
+    boundary = pos[:y]+ship[:size]-1
+    if boundary >= $BOARD_SIZE[:height]
+      deploy ship
+    else
+      op = true
+      (pos[:y]..(pos[:y]+ship[:size]-1)).each{|y|
+        unless $BOARD[pos[:x]][y]=='.'
+          op = false
+          break
+        end
+      }
+      if op
+        (pos[:y]..(pos[:y]+ship[:size]-1)).each{|y|
+          $BOARD[pos[:x]][y] = ship[:label]
+        }
+      else
+        deploy ship
+      end
+    end
+  end
+end
+
+$SHIPS.each {|ship|
+  deploy ship
+}
+
+# $BOARD.each{|row|
+#   puts row.join(' ')
+# }
+
+sinks = []
+sinks_board = Array.new($BOARD_SIZE[:width]) {Array.new($BOARD_SIZE[:height]) {'.'}}
+cnt = 0
+print "Enter Position x y: "
+while g = gets.chomp
+  cnt += 1
+  y,x = g.split(/\s/).map(&:to_i)
+  # puts "#{x}, #{y}"
+  if [:A, :B, :C, :S, :D].include?($BOARD[x][y])
+    sinks << $BOARD[x][y] unless sinks.include?($BOARD[x][y])
+    sinks_board[x][y] = $BOARD[x][y]
+  else
+    sinks_board[x][y] = 'X'
+  end
+  puts '  '+(0..9).to_a.join(' ')
+  sinks_board.each.with_index{|row, idx|
+    puts "#{idx} #{row.join(' ')}"
+  }
+  if sinks.size >= 5
+    break
+  else
+    print "Enter Position x y: "
+  end
+end
+puts "YOU WIN"
